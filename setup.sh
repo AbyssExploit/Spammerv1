d2b4c03fbed5ce0b098b8a4fc309521e5227b319
AbyssWalker <63506758+AbyssExploit@users.noreply.github.com>
2020-10-02 19:36:05 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/system/xbin/bash
+
+figlet Abyss Walker | lolcat
+echo " Tunggu Sebentar "
+
+apt install bash -y
+apt install python -y
+apt install figlet -y
+apt install ruby -y
+gem install lolcat
+apt install toilet -y
+apt install python2 -y
+apt install cowsay -y
+pip2 install requests
+pip2 install mechanize
+pip install requests
+pip install mechanize
+
+toilet --metal -f future -t " INSTALL BERES"
